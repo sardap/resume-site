ecafc4032cee19e23f95254c4cef1e2829c0f8c2
sardap <sardap@users.noreply.github.com>
2021-01-25 08:08:38 +1100

    inital commit

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+
+  rules: {
+    "no-console": "off",
+    "no-debugger": "off",
+    "vue/max-len": "error"
+  },
+
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint"
+  ]
+};
