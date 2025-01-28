c4c1dda02e84638ca4c6e5a969a3ad49853ad376
sardap <paul@sarda.dev>
2025-01-28 21:52:35 +1100

    Testing disalbing roman clock

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import wasmPack from 'vite-plugin-wasm-pack'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
 
